3b0d6e462c11f42f4883270ea3cbc7a27e54a834
prateek-singh-inviz <115148773+prateek-singh-inviz@users.noreply.github.com>
2022-10-10 14:01:40 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,12 +3,12 @@ echo $gcp_credential > temp.json
 sudo snap install google-cloud-cli --classic
 export SERVICE_ACCOUNT='ner-275@onyx-principle-364411.iam.gserviceaccount.com'
 export REGION='asia-south1'
-export ENDPOINT_NAME='ner-testing-v1'
-export MODEL_NAME1='ner-v01-m1'
-export MODEL1_ID='new-testing-1'
+export ENDPOINT_NAME='ner-testing-v2'
+export MODEL_NAME1='ner-v01-m2'
+export MODEL1_ID='new-testing-2'
 # export MODEL_NAME2='Model_3'
 # export MODEL2_ID='MY_Model_03'
-export ENDPOINT_ID='11908231'
+export ENDPOINT_ID='11908232'
 export CONTAINER_IMAGE='gcr.io/onyx-principle-364411/ner-model-inference:v1'
 export PROJECT='onyx-principle-364411'
 gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=temp.json --project=$PROJECT
