d172edcf059b97dde79dd515af908b8a588e4a61
prateek-singh-inviz <115148773+prateek-singh-inviz@users.noreply.github.com>
2022-10-12 13:19:48 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,12 +3,12 @@ echo $gcp_credential > temp.json
 sudo snap install google-cloud-cli --classic
 export SERVICE_ACCOUNT='nermodelfull@test-project-365213.iam.gserviceaccount.com'
 export REGION='us-central1'
-export ENDPOINT_NAME='ner-testing-v5'
-export MODEL_NAME1='ner-v01-m5'
-export MODEL1_ID='new-testing-5'
+export ENDPOINT_NAME='ner-testing-v6'
+export MODEL_NAME1='ner-v01-m6'
+export MODEL1_ID='new-testing-6'
 # export MODEL_NAME2='Model_3'
 # export MODEL2_ID='MY_Model_03'
-export ENDPOINT_ID='11908235'
+export ENDPOINT_ID='11908236'
 export CONTAINER_IMAGE='gcr.io/test-project-365213/ner-model-inference-v2:latest'
 export PROJECT='test-project-365213'
 export GOOGLE_APPLICATION_CREDENTIALS=temp.json
